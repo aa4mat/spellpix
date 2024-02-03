0e72bf5842f7b6781ade36a8ca72fc9a642b72b4
djletts <158646696+djletts@users.noreply.github.com>
2024-02-03 11:20:33 -0500

    Add files via upload

--- spell_check.py
+++ spell_check.py
@@ -1,9 +1,24 @@
 import textblob
+import json
 sentence = ['inconsistn teqnique', 'rugh estimite']
+word_list = []
+for item in sentence:
+    word_list.append("")
+    for char in item:
+        if (ord(char) > 64 and ord(char) < 91) or (ord(char) > 96 and ord(char) < 123):
+            word_list[-1] = word_list[-1] + char
+        else:
+            word_list.append("")
+    
 textblob_objects = list()
-for word in sentence:
+incorrect_words = list()
+for word in word_list:
+    incorrect_words.append(word)
     textblob_objects.append(textblob.TextBlob(word))
 
+data = {}
+for i in range(len(textblob_objects)):
+    data[incorrect_words[i]] = str(textblob_objects[i].correct())
 
-for textblob_object in textblob_objects:
-    print(textblob_object.correct())
+with open('data.json', 'w') as f:
+    json.dump(data, f)
